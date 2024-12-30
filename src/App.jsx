24fab3c7077184b4f755104dd7cc41690d2ead4b
phuongnam <nguyenphuongnam284@gmail.com>
2024-12-30 11:16:04 +0700

    base structure code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
+
+import Layout from '@components/Layout/Layout'
 import './App.css'
+import Header from '@components/Header/Header'
+import Footer from '@components/Footer/Footer'
 
 function App() {
-
   return (
-    <div>E-commerce</div>
+    <Layout>
+      <Header />
+      <Footer />
+    </Layout>
   )
 }
 
