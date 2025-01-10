22c9f350575bcbfb4c0fcb6054980901c295dccf
phuongnam <nguyenphuongnam284@gmail.com>
2025-01-10 21:14:34 +0700

    call api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import routers from "@/routers/routers"
-import { Suspense } from "react"
-import { SideBarProvider } from "@/context/SideBarProvider"
-import SideBar from "@components/SideBar/SideBar"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routers from "@/routers/routers";
+import { Suspense } from "react";
+import { SideBarProvider } from "@/context/SideBarProvider";
+import SideBar from "@components/SideBar/SideBar";
+import { ToastProvider } from "@/context/ToastProvider";
 
 function App() {
   return (
+    <ToastProvider>
       <SideBarProvider>
-        <SideBar/>
+        <SideBar />
         <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {routers.map((item, index)=>{
-              return <Route path={item.path} element={<item.component/>} key={index} />
-            })}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              {routers.map((item, index) => {
+                return (
+                  <Route
+                    path={item.path}
+                    element={<item.component />}
+                    key={index}
+                  />
+                );
+              })}
             </Routes>
           </Suspense>
         </BrowserRouter>
       </SideBarProvider>
-  )
+    </ToastProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./style.module.scss";
 import InputCommon from "@components/InputCommon/InputCommon";
 import Button from "@components/Button/Button";
 
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { ToastContext } from "@/context/ToastProvider";
+import { register } from "@/apis/authService";
 
 const Login = () => {
   const { container, title, boxRemember, boxBtn, lostPass } = styles;
   const [isRegister, setIsRegister] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useContext(ToastContext);
   const handleToggle = () => {
     setIsRegister(!isRegister);
     formik.resetForm();
@@ -29,8 +33,23 @@ const Login = () => {
         "Password must match"
       ),
     }),
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      const { email: username, password } = values;
+      if (isLoading) {
+        return;
+      }
+      if (isRegister) {
+        setIsLoading(true);
+        await register({ username, password })
+          .then((res) => {
+            toast.success(res.data.message);
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            toast.error(err.response.data.message);
+            setIsLoading(false);
+          });
+      }
     },
   });
 
@@ -70,7 +89,12 @@ const Login = () => {
           </div>
         )}
         <div className={boxBtn}>
-          <Button content={isRegister ? "REGISTER" : "LOGIN"} type="submit" />
+          <Button
+            content={
+              isLoading ? "LOADING..." : isRegister ? "REGISTER" : "LOGIN"
+            }
+            type="submit"
+          />
         </div>
       </form>
       <div className={boxBtn} onClick={handleToggle}>
